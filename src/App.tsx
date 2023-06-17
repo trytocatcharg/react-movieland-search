d02e93422effea9fe2632e0e9b4feef8b57295dc
trytocatch <blockchain@DESKTOP-C6LHPJ9.localdomain>
2023-06-17 18:15:12 +0200

    fix https issue

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import MovieCard from './MovieCard';
 import { MovieCardModel } from './models/movies';
 
 
-const API_URL = `http://www.omdbapi.com/?apikey=${import.meta.env.VITE_APP_API_KEY}`;
+const API_URL = `https://www.omdbapi.com/?apikey=${import.meta.env.VITE_APP_API_KEY}`;
 
 const App = () => {
 const [movies ,setMovies] = useState([] as MovieCardModel[]);
